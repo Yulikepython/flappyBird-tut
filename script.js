f64246998573390a1dacc43fb36e25bead23ac2c
Yulikepython <admin@itc.tokyo>
2021-11-18 23:11:04 +0900

    up to 12:31

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+import {updateBird} from "./bird.js"
+
 document.addEventListener("keypress", handleStart, {once: true});
 
-function updateLoop(time){
-    console.log(time)
-    window.requestAnimationFrame(updateLoop)
+let lastTime
 
+function updateLoop(time){
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(updateLoop)
+        return
+    }
+    const delta = time - lastTime
+    updateBird(delta)
+    lastTime = time
+    window.requestAnimationFrame(updateLoop)
 }
 
 function handleStart(){
